956c376e18cdaddd14b55dea96e2a5c56f3f4fe8
Brian Obot <brianobot@gmail.com>
2025-07-04 10:24:26 +0100

    refactor: Improve code formatting and organization in multiple files

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,12 @@ mod player;
 #[allow(unused)]
 use player::*;
 
-
 fn main() {
     App::new()
         // this adds features like Window management to the application
-        .add_plugins(DefaultPlugins
-            .set(
-                WindowPlugin {
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: String::from("Space Invaders 🚀"),
                         position: WindowPosition::Centered(MonitorSelection::Primary),
@@ -30,9 +29,9 @@ fn main() {
                         ..Default::default()
                     }),
                     ..Default::default()
-            })
-            // this prevents the pixel from becoming blurry with the standard by linear settings
-            .set(ImagePlugin::default_nearest())
+                })
+                // this prevents the pixel from becoming blurry with the standard by linear settings
+                .set(ImagePlugin::default_nearest()),
         )
         .add_plugins(ResolutionPlugin)
         .add_plugins(GamePlugin)
